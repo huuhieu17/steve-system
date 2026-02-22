271906c4bd7e30ce2d876cb5547a75c1b630d9f4
Nguyen Huu Hieu <huuhieu1711@gmail.com>
2026-02-23 02:44:18 +0700

    fix: change read env vite

--- shell-app/vite.config.ts
+++ shell-app/vite.config.ts
@@ -2,10 +2,10 @@ import federation from '@originjs/vite-plugin-federation';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import { defineConfig } from 'vite';
-export default defineConfig(({}) => {
-
-  const API_URL = `${process.env.VITE_CONSOLE_API_URL ?? 'http://localhost:4173'}`
+import { defineConfig, loadEnv } from 'vite';
+export default defineConfig(({mode}) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  const API_URL = `${env.VITE_CONSOLE_API_URL ?? 'http://localhost:4173'}`
   return {
     server: {
       proxy: {
@@ -13,7 +13,6 @@ export default defineConfig(({}) => {
           target: API_URL,
           changeOrigin: true,
           rewrite: (path) => {
-            console.log('Proxying request to:', API_URL + path);
             return path.replace(/^\/console/, '')
           },
         },
